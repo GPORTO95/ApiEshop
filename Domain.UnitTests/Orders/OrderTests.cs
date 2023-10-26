9489cd1154f16ae6acce5c13971198e83a719df0
Gabriel Porto <gr_porto@hotmail.com>
2023-10-25 21:40:27 -0300

    `class:` The Best Way To Write Parameterized Tests In .NET Using xUnit

---
+++ Domain.UnitTests/Orders/OrderTests.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using Domain.Customers;
+using Domain.Orders;
+
+namespace Domain.UnitTests.Orders;
+
+public class OrderTests
+{
+    [Theory]
+    [ClassData(typeof(OrderCreateTestData))]
+    public void Create_Should_RaiseDomainEvent(CustomerId customerId)
+    {
+        // Act
+        var order = Order.Create(customerId);
+
+        // Assert
+        Assert.NotEmpty(order.GetDomainEvents().OfType<OrderCreatedDomainEvent>());
+    }
+}
+
+public class OrderCreateTestData : TheoryData<CustomerId>
+{
+    public OrderCreateTestData()
+    {
+        var faker = new Faker();
+
+        Add(new CustomerId(faker.Random.Guid()));
+    }
+}
---
+++ Domain.UnitTests/Products/SkuTests.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Products;
+
+namespace Domain.UnitTests.Products;
+
+public class SkuTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void Create_Should_ReturnNull_WhenValueIsNullOrEmpty(string? value)
+    {
+        // Act
+        var sku = Sku.Create(value);
+
+        // Assert
+        Assert.Null(sku);
+    }
+
+    public static IEnumerable<object[]> InvalidSkuLengthDataMethod() => new List<object[]>
+    {
+        new object[] { "invalid_sku" },
+        new object[] { "invalid_sku_1" },
+        new object[] { "invalid_sku_2" }
+    };
+
+    [Theory]
+    [MemberData(nameof(InvalidSkuLengthDataMethod))]
+    public static void Create_Should_ReturnNull_WhenValueLengthIsInvalid(string value)
+    {
+        // Act
+        var sku = Sku.Create(value);
+
+        // Assert
+        Assert.Null(sku);
+    }
+}
